cb2a0d3deae039b15cffa4ce82d9b545d12d3555
KrisQ <kquemener1@gmail.com>
2024-12-12 20:48:55 -0600

    feat: main classes

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
+AsteroidField.containers = (updatable,)
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    AsteroidField()
     dt = 0
     while True:
         for event in pygame.event.get():
           if event.type == pygame.QUIT:
             return
         screen.fill((0, 0, 0))
+        for updatable_sprite in updatable:
+           updatable_sprite.update(dt)
+        for drawable_sprite in drawable:
+           drawable_sprite.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
